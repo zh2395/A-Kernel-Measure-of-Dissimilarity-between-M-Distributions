7c06bc9129e819c1449e4ee9e27f4e73665c73a7
zh2395 <72772154+zh2395@users.noreply.github.com>
2023-10-31 20:36:25 -0400

    Update bias_mse.R

--- bias_mse.R
+++ bias_mse.R
@@ -1,11 +1,18 @@
+# The script reproduces the two figures (Figures 6-7) in Appendix D.3 
+# on the bias and MSE of the empirical KMD
+
+# The number of replications used to estimate the mean and MSE
 B = 10000
 num_cores = 7
 library(ggplot2)
 
 # eta = 1 M = 2
+# Consider dimension = 2, n = 300
 d = 2
 n = 300
+# Store the mean for k = 1,..,30 (number of nearest neighbors)
 mean_eta_n300d2 = rep(NA,30)
+# Store the mse for k = 1,..,30
 mse_eta_n300d2 = rep(NA,30)
 for (k in 1:30) {
   compute_eta = function(i) {
@@ -20,6 +27,7 @@ for (k in 1:30) {
   mse_eta_n300d2[k] = mean((tmp - 1)^2)
 }
 
+# Consider dimension = 2, n = 60
 n = 60
 mean_eta_n60d2 = rep(NA,30)
 mse_eta_n60d2 = rep(NA,30)
@@ -36,6 +44,7 @@ for (k in 1:30) {
   mse_eta_n60d2[k] = mean((tmp - 1)^2)
 }
 
+# Consider dimension = 20, n = 60
 d = 20
 n = 60
 mean_eta_n60d20 = rep(NA,30)
@@ -53,6 +62,7 @@ for (k in 1:30) {
   mse_eta_n60d20[k] = mean((tmp - 1)^2)
 }
 
+# Consider dimension = 20, n = 300
 n = 300
 mean_eta_n300d20 = rep(NA,30)
 mse_eta_n300d20 = rep(NA,30)
@@ -69,11 +79,11 @@ for (k in 1:30) {
   mse_eta_n300d20[k] = mean((tmp - 1)^2)
 }
 
-# eta = 1 M = 3
 
+# eta = 1 M = 3
+# Repeat the same procedure as above
 d = 2
 n = 300
-
 mean_eta_n300d2M3 = rep(NA,30)
 mse_eta_n300d2M3 = rep(NA,30)
 for (k in 1:30) {
@@ -108,7 +118,6 @@ for (k in 1:30) {
   mean_eta_n300d20M3[k] = mean(tmp)
   mse_eta_n300d20M3[k] = mean((tmp - 1)^2)
 }
-
 
 n = 60
 d = 2
@@ -149,6 +158,7 @@ for (k in 1:30) {
 
 
 # eta = 0.5 M = 2
+# Repeat the same procedure as above
 d = 2
 n = 300
 
@@ -217,6 +227,7 @@ for (k in 1:30) {
 
 
 # eta = 0.5 M = 3
+# Repeat the same procedure as above
 d = 2
 n = 300
 mean_eta0.5_n300d2M3 = rep(NA,30)
@@ -288,6 +299,7 @@ for (k in 1:30) {
 }
 
 # eta = 0.1 M = 2
+# Repeat the same procedure as above
 d = 2
 n = 300
 mean_eta0.1_n300d2 = rep(NA,30)
@@ -349,7 +361,10 @@ for (k in 1:30) {
   mean_eta0.1_n60d2[k] = mean(tmp)
   mse_eta0.1_n60d2[k] = mean((tmp - 0.1)^2)
 }
+
+
 # eta = 0.1 M = 3
+# Repeat the same procedure as above
 d = 2
 n = 300
 mean_eta0.1_n300d2M3 = rep(NA,30)
@@ -416,9 +431,12 @@ for (k in 1:30) {
   mse_eta0.1_n60d2M3[k] = mean((tmp - 0.1)^2)
 }
 
+
+# A color blind friendly palette
 cbp1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73",
           "#0072B2", "#F0E442", "#D55E00", "#CC79A7")
-# d = 20
+# Generate the six sub-plots and combine them together
+# Mean of the estimators
 p_eta1M2 = ggplot(data.frame(k = rep(1:30,4),
                              d.n = c(rep("(2,60)",30),rep("(2,300)",30),rep("(20,60)",30),rep("(20,300)",30)),
                              Mean = c(mean_eta_n60d2,mean_eta_n300d2,mean_eta_n60d20,mean_eta_n300d20)),
@@ -488,12 +506,13 @@ p_eta0.1M3 = ggplot(data.frame(k = rep(1:30,4),
   theme(plot.title = element_text(hjust = 0.5),legend.box.margin = margin(0, -8, 0, 0)) +
   ylim(c(0,0.1))
 
+# Combine the plots together
 ggpubr::ggarrange(p_eta1M2, p_eta0.5M2, p_eta0.1M2,
                   p_eta1M3, p_eta0.5M3, p_eta0.1M3,
                   ncol = 3, nrow = 2, widths = c(1,0.9,1.25) )
 
 
-# MSE
+# MSE of the estimators
 p_eta1M2_MSE = ggplot(data.frame(k = rep(1:30,4),
                              d.n = c(rep("(2,60)",30),rep("(2,300)",30),rep("(20,60)",30),rep("(20,300)",30)),
                              MSE = c(mse_eta_n60d2,mse_eta_n300d2,mse_eta_n60d20,mse_eta_n300d20)),
@@ -557,7 +576,7 @@ p_eta0.1M3_MSE = ggplot(data.frame(k = rep(1:30,4),
   scale_colour_manual(values=cbp1) +
   geom_point(aes(color = d.n,shape=d.n)) + 
   theme(plot.title = element_text(hjust = 0.5),legend.box.margin = margin(0, -8, 0, 0))
-
+# Combine the plots together
 ggpubr::ggarrange(p_eta1M2_MSE, p_eta0.5M2_MSE, p_eta0.1M2_MSE,
                   p_eta1M3_MSE, p_eta0.5M3_MSE, p_eta0.1M3_MSE,
                   ncol = 3, nrow = 2, widths = c(1,0.9,1.25) )
